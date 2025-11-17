5dd7f99cf17a36439f1cd1c3e7495a1b96ec885b
anshu340 <anshukarki533@gmail.com>
2025-11-17 12:02:59 +0545

    Task 1 completed

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Workshop4
+﻿using System;
+using Workshop4;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        //Task 1
+
+        // Creating first object
+        Student s1 = new Student();
+        s1.name = "Anshu";
+        s1.age = 21;
+        s1.address = "Belbari";
+
+        // Creating second object
+        Student s2 = new Student();
+        s2.name = "Ab";
+        s2.age = 18;
+        s2.address = "Belbari - 2";
+
+        // Display object 1 values
+        Console.WriteLine("Student 1 Details:");
+        Console.WriteLine("Name: " + s1.name);
+        Console.WriteLine("Age: " + s1.age);
+        Console.WriteLine("Address: " + s1.address);
+
+        // Display object 2 values
+        Console.WriteLine("\nStudent 2 Details:");
+        Console.WriteLine("Name: " + s2.name);
+        Console.WriteLine("Age: " + s2.age);
+        Console.WriteLine("Address: " + s2.address);
+
+        // Print static field
+        Console.WriteLine("\nCollege Name : " + Student.collegeName);
     }
 }
--- Workshop4/Student.cs
+++ Workshop4/Student.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Workshop4
 {
-    internal class Student
+    //Task 1
+    public class Student
     {
+        // 3 instance fields
+        public string name;
+        public int age;
+        public string address;
+
+        // 1 static field
+        public static string collegeName = "Itahari international College";
+
     }
 }
